e84f743e24c57e80199769491a16878e12d68392
Jose Giraudo <josegiraudo7@gmail.com>
2023-09-28 11:26:23 -0300

    Creacion de las clases (Entidades) y del HelperDAO

---
+++ ProyectoCarpinteria/Datos/HelperDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCarpinteria.Datos
+{
+    internal class HelperDAO
+    {
+        // Aca usamos el patron de diseño Singleton
+    }
+}
